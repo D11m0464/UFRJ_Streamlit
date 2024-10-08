b956a2451d2aea30c0b00cf1fdaec852880f776d
D11m0464 <crc.santos@gmail.com>
2024-10-08 20:12:30 -0300

    Update sei_rel.py

--- sei_rel.py
+++ sei_rel.py
@@ -1,26 +1,17 @@
-import streamlit as st  
+import streamlit as st 
+ 
 
 #st.set_page_config(layout="wide")
 
 coluna_esquerda,coluna_meio,coluna_direita=st.columns([1,1,1])
 
-relatorio=coluna_esquerda.selectbox('Relatórios', options=['Listagem_Protocolos_Abertos','Listagem_SEI_Assuntonaodisponibilizado','Listagem_SEI_EstatisticoUnidade','Listagem_SEI_ProcessosGerados'])
+relatorio=coluna_esquerda.selectbox('Relatórios', options=['Protocolos_Abertos','Assunto não disponibilizado','Estatistico da Unidade','Processos Gerados'])
 
 
-if relatorio == 'Listagem_Protocolos_Abertos':
-    container_dash,container_lateral=st.columns([5,1],gap='small',vertical_alignment='top')
+if relatorio == 'Protocolos_Abertos':
+       container_dash,container_lateral=st.columns([5,1],gap='small',vertical_alignment='top')
 
-    #container_dash.container().pa(src='https://ccbi.tic.ufrj.br/pentaho/api/repos/%3Ahome%3Acarloscarvalho%3ASEI%3ARelatorio%3AListagem_Protocolos_Abertos.prpt/viewer', width=1500,height=1200,scrolling=True)
-
-
-if relatorio == 'Listagem_SEI_Assuntonaodisponibilizado':
-    container_dash,container_lateral=st.columns([5,1],gap='small',vertical_alignment='top')
-
-    #container_dash.container()._iframe(src='https://ccbi.tic.ufrj.br/pentaho/api/repos/%3Ahome%3Acarloscarvalho%3ASEI%3ARelatorio%3AListagem_SEI_Assuntonaodisponibilizado.prpt/viewer', width=1500,height=1200,scrolling=True)
+       container_dash.container()._iframe(src='https://dashboard.ccbi.tic.ufrj.br/public/dashboard/2c051ef0-22b4-489d-a7c0-ca1b94df7811', width=1500,height=1200,scrolling=True) 
+ 
 
 
-
-#import streamlit.components as componente
-
-
-#componente.
